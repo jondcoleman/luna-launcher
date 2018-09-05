3c0ece9563c82720f9d5dfd0013b7bfa72601ac8
Jonathan Coleman <jcoleman3307@gmail.com>
2018-09-05 13:13:13 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Autocomplete from './components/Autocomplete'
+
+class App extends Component {
+  render() {
+    return <div className="App">Hello World</div>
+  }
+}
+
+export default App
